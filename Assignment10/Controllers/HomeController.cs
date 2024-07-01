1fbd199dfeadd58c6e43d9be00fc6d63701a6d61
sameer singh <v-samesingh@microsoft.com>
2024-07-01 18:41:23 +0530

    Add project files.

---
+++ Assignment10/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Assignment10.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment10.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Content("Welcome to the Best Bank");
+        }
+
+        [Route("/account-details")]
+        public IActionResult AccountDetails()
+        {
+            Person person = new Person() { acccountNumber = 1001, accountHolderName = "Example Name", currentBalance = 5000 };
+            return Json(person);
+        }
+
+        [Route("/account-statement")]
+        public IActionResult AccountStatement()
+        {
+            return File("carimg.jpg", "image/jpeg");
+        }
+
+        [Route("/get-current-balance/{accountNumber:int}")]
+        public IActionResult GetCurrentBalance()
+        {
+            if (Request.RouteValues.ContainsKey("accountNumber"))
+            {
+                int accountNumber = Convert.ToInt32(Request.RouteValues["accountNumber"]);
+
+
+                if (accountNumber == 1001)
+                {
+                    return Content("5000");
+                }
+                else
+                {
+                    return BadRequest("Account Number should be 1001");
+                }
+            }
+
+            else
+            {
+                return BadRequest("Account Number should be supplied");
+            }
+
+        }
+
+    }
+}
---
+++ Assignment10/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment10.Models
+{
+    public class Person
+    {
+        public int acccountNumber { get; set; }
+        public string? accountHolderName { get; set; }
+
+        public double currentBalance { get; set; }
+
+    }
+}
